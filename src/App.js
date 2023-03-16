55ef328594ad3f5909d59b5a25d1fa50f45b8fe6
Cristina Hernández <achh17@gmail.com>
2023-03-16 17:59:19 -0500

    I added reducers using mapStateToProps and mapDispatchToProps

--- src/App.js
+++ src/App.js
@@ -1,36 +1,47 @@
 import { useEffect, useState } from 'react';
 import { Col } from 'antd';
+import { connect } from 'react-redux';
 import { getPokemon } from './api';
 import 'antd/dist/reset.css';
 import Searcher from '../src/components/Searcher';
 import './App.css';
 import PokemonList from '../src/components/PokemonList';
 import logo from './statics/logo.svg'
+import { setPokemons } from './actions/index.js';
+import { setPokemons as setPokemonsActions } from './actions'
 
 
 
-function App() {
-const [pokemons, setPokemons] = useState([]);
-
-  useEffect (() => {
-    const fecthPokemons = async () => { 
+function App({ pokemons, setPokemons }) {
+  console.log("🚀 ~ file: App.js ~ line 12 ~ App ~ pokemons", pokemons)
+  useEffect(() => {
+    const fetchPokemons = async () => {
       const pokemonsRes = await getPokemon();
-      setPokemons(pokemonsRes)
+      setPokemons(pokemonsRes);
     };
-    fecthPokemons();
+
+    fetchPokemons();
   }, []);
 
   return (
-    <div className="App">
+    <div className='App'>
       <Col span={4} offset={10}>
-      <img src={logo} alt='pokedux' />
+        <img src={logo} alt='Pokedux' />
       </Col>
       <Col span={8} offset={8}>
         <Searcher />
       </Col>
-      <PokemonList pokemons={pokemons}/>
+      <PokemonList pokemons={pokemons} />
     </div>
   );
 }
 
-export default App;
+const mapStateToProps = (state) => ({
+  pokemons: state.pokemons,
+});
+
+const mapDispatchToProps = (dispatch) => ({
+  setPokemons: (value) => dispatch(setPokemonsActions(value)),
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(App);
