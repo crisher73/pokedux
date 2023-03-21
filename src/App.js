7ff8c60858138d5db91ca158d103d3cc8a1faf0b
Cristina Hernández <achh17@gmail.com>
2023-03-21 15:57:23 -0500

    I added immutable using methods like getIn setIn

--- src/App.js
+++ src/App.js
@@ -12,8 +12,8 @@ import 'antd/dist/reset.css';
 
 function App() {
 
-  const pokemons = useSelector((state) => state.pokemons);
-  const loading = useSelector((state) => state.loading);
+  const pokemons = useSelector((state) => state.get('pokemons')).toJS();
+  const loading = useSelector((state) => state.get('loading'));
   const dispatch = useDispatch();
 
   useEffect(() => {
